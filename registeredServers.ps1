d8f0c237639ae519f62248df146ffa51c6735689
Phil Factor <phil.factor@simple-talk.com>
2012-08-22 14:10:12 +0100

    First commit
    initial working version of Registered Servers showing how to get all the
    server logins

---
+++ registeredServers.ps1
@@ -0,0 +1,25 @@
+﻿#Load SMO assemblies
+$CentralManagementServer = "andrewctest.testnet.red-gate.com\sql2008"
+$MS='Microsoft.SQLServer'
+@('.SMO', '.Management.RegisteredServers', '.ConnectionInfo') |
+     foreach-object {if ([System.Reflection.Assembly]::LoadWithPartialName("$MS$_") -eq $null) {"missing SMO component $MS$_"}}
+
+$connectionString = "Data Source=$CentralManagementServer;Initial Catalog=master;Integrated Security=SSPI;"
+$sqlConnection = new-object System.Data.SqlClient.SqlConnection($connectionString)
+$conn = new-object Microsoft.SqlServer.Management.Common.ServerConnection($sqlConnection)
+$CentralManagementServerStore = new-object Microsoft.SqlServer.Management.RegisteredServers.RegisteredServersStore($conn)
+
+#$CentralManagementServerStore.ServerGroups
+
+$My="$ms.Management.Smo" #
+$CentralManagementServerStore.ServerGroups["DatabaseEngineServerGroup"].ServerGroups["BackUpEverythin"].RegisteredServers|
+   Foreach-object {new-object ("$My.Server") $_.ServerName } | # create an SMO server object
+     Where-Object {$_.ServerType -ne $null} | # did you positively get the server?
+       Foreach-object {$_.Logins } | #logins for every server successfully reached 
+           Select-object @{Name="Server"; Expression={$_.parent}}, Name, DefaultDatabase , CreateDate, DateLastModified   |
+              format-table
+
+
+"did that work"
+
+
